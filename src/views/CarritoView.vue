3690871ff9905502d91e674019d3b4b040a0ee47
MilanessaGit <miguel.aguirre.v12@gmail.com>
2023-09-17 21:45:33 -0400

    Creacion del proyecto y una tabla basica del carrito

---
+++ src/views/CarritoView.vue
@@ -0,0 +1,50 @@
+<template>
+    <h1>{{ title }}</h1>
+    <input type="text" v-model="title">
+    <hr>
+    <table border="1">
+        <tr>
+            <td>NOMBRE</td>
+            <td>PRECIO</td>
+            <td>ACTION</td>
+        </tr>
+        <tr v-for="prod in productos" :key="prod">
+            <td>{{ prod.nombre }}</td>
+            <td>{{ prod.precio }}</td>
+            <td><button type="button" @click="addCarrito()">agregar</button></td>
+        </tr>
+    </table>
+
+    <hr>
+    <table border="1">
+        <tr>
+            <td>NOMBRE</td>
+            <td>PRECIO</td>
+            <td>ACTION</td>
+        </tr>
+        <tr v-for="car in carrito" :key="car">
+            <td>{{ car.nombre }}</td>
+            <td>{{ car.precio }}</td>
+            <td><button type="button" @click="quitar()">quitar</button></td>
+        </tr>
+    </table>
+
+</template>
+
+<script>
+    import { ref } from "vue"
+    export default{
+        // composicion api
+        setup(){
+            const title = ref("Mi Carrito de Compras")
+            const productos = ref([{nombre: "teclado", precio: 150 },
+                                    {nombre: "monitor", precio: 250 }]);
+            const carrito = ref([])
+            const addCarrito = (prod) => {
+
+            }
+                                    
+            return { title, productos, addCarrito }
+        }
+    }
+</script>
